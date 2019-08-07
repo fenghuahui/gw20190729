a30ad509af273b9242d56466c7501be91257e616
853187348@qq.com <853187348@qq.com>
2019-08-07 09:56:22 +0800

    分类列表

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,39 @@
-// pages/category/index.js
+import { request } from '../../request/index.js'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    navList:[],
+    rightList:[],
+    //选中颜色
+    currentIndex:0,
+    //滚动条位置
+    scrollTop:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  Cates:[],
+  onLoad(){
+  this.getNavList();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getNavList(){
+    request({url:'/categories'})
+    .then((res)=>{
+      this.Cates = res
+      console.log(res);
+      const navList = this.Cates.map(v=>({cat_id:v.cat_id,cat_name:v.cat_name}))
+      const rightList = this.Cates[0].children
+      this.setData({
+        navList,
+        rightList
+      })
+    })   
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleMenuChange(e){
+  console.log(e);
+  // 选中那个根据index切换
+  const {index} = e.currentTarget.dataset; 
+  const rightList = this.Cates[index].children
+  this.setData({
+    currentIndex:index,
+    rightList,
+    scrollTop:0
+  })
   }
 })
